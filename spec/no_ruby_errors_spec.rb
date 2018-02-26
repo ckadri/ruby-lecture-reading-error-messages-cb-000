effd955977c1925147d3e0801eff7f40a257169c
Carol Kadri <ckadri48@gmail.com>
2018-02-26 19:41:07 +0000

    automatically backed up by learn

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -4,7 +4,7 @@ describe "Ruby Errors" do
     it 'raises a NameError when encountering undefined barewords' do
       expect{
         load './lib/a_name_error.rb'
-      }.to_not raise_error 
+      }.to_not raise_error
     end
   end
 
